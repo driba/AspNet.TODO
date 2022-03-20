6062ec1896d53c4a679b3484df7fc08f849b5514
driba <dorian.ribaric@gmail.com>
2022-03-20 12:02:13 +0100

    Add project files.

---
+++ AspNet.TODO/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNet.TODO.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
