1bbb8ef96c2f74cc63c5adb3320f55b3c72702c8
driba <dorian.ribaric@gmail.com>
2022-03-21 00:10:20 +0100

    Simulirana baza u repozitoriju

--- AspNet.TODO/Models/Todo.cs
+++ AspNet.TODO/Models/Todo.cs
@@ -1,11 +1,21 @@
-﻿namespace AspNet.TODO.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNet.TODO.Models
 {
     public class Todo
     {
+        [DisplayName("ID")]
         public int Id { get; set; }
+
+        [DisplayName("Naziv zadatka")]
         public string Name { get; set; }
+
+        [DisplayName("Rok zadatka")]
+        //[DataType(DataType.Date)]
         public DateTime Deadline { get; set; }
-        public bool Status { get; set; }
+
+        public string Status { get; set; }
 
     }
 }
---
+++ AspNet.TODO/Repository/TodoRepository.cs
@@ -0,0 +1,61 @@
+﻿using AspNet.TODO.Models; 
+
+namespace AspNet.TODO.Repository
+{
+    public class TodoRepository
+    {
+        private List<Todo> todo_list;
+
+        public TodoRepository()
+        {
+            if (todo_list == null)
+            {
+                todo_list = new List<Todo>();
+            }
+        }
+
+        private void SimulateData()
+        {
+            Todo task1 = new Todo()
+            {
+                Id = 1,
+                Name = "Planiraj tjedni šoping",
+                Deadline = DateTime.Now
+            };
+
+            Todo task2 = new Todo()
+            {
+                Id = 2,
+                Name = "Pošalji mail",
+                Deadline = DateTime.Now
+            };
+
+            Todo task3 = new Todo()
+            {
+                Id = 3,
+                Name = "Kupi poklon mami",
+                Deadline = DateTime.Now
+            };
+
+            Todo task4 = new Todo()
+            {
+                Id = 5,
+                Name = "Predaj ispit modula 8 u ponedjeljak do 15h",
+                Deadline = new DateTime(2022, 3, 21)
+            };
+
+            Todo task5 = new Todo()
+            {
+                Id = 8,
+                Name = "Godišnji!",
+                Deadline = new DateTime(2022, 7, 1)
+            };
+
+            todo_list.Add(task1);
+            todo_list.Add(task2);
+            todo_list.Add(task3);
+            todo_list.Add(task4);
+            todo_list.Add(task5);
+        }
+    }
+}
